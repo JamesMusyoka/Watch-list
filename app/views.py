c9648878f01816ec93375eb345eb314eb0216d96
JamesMusyoka <jamesmu475@gmail.com>
2020-09-30 02:10:06 +0300

    Jinja templates allow us to add some logic to our application

--- app/views.py
+++ app/views.py
@@ -8,7 +8,7 @@ def index():
     View root page function that returns the index page and its data
     '''
 
-    message = 'Hello World'
+    title = 'Home - Welcome to The best Movie Review Website Online'
     return render_template('index.html',message = message)
 
 @app.route('/movie/<movie_id>')
