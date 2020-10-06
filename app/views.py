329717ea0167c6ff17ef1d7e4cd10e6c9ed760fa
JamesMusyoka <jamesmu475@gmail.com>
2020-10-06 22:53:38 +0300

    We first import the Review class from our models folder. We also import the ReviewForm class from our forms file.

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from flask import render_template,request,redirect,url_for
 from app import app
 from .request import get_movies,get_movie,search_movie
+from .models import reviews
+from .forms import ReviewForm
+Review = reviews.Review
 
 @app.route('/')
 def index():
